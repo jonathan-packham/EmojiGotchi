295f816acb9eb55e70e0a776bdfd6417aef90d46
p4ckman <90421233+jonathan-packham@users.noreply.github.com>
2023-02-10 00:38:59 -0700

    newest version, still not quite working

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ActivityIndicator, Pressable, StyleSheet, Text, View} from 'react-native';
+import {Card} from 'react-native-paper';
+import {CleanTimer, DeathTimer, HungerTimer, LonelyTimer} from 'C:/Users/packh/mobileApp/emojiPetv4/emojiTimers';
+import {CleanDisplay, DefaultDisplay, DirtyDisplay, DyingDisplay, FedDisplay, HappyDisplay, HungerDisplay, LonelyDisplay} from 'C:/Users/packh/mobileApp/emojiPetv4/emojiDisplay';
 
 export default function App() {
-  return (
+
+  return ( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.timerContainer}>
+        <HungerTimer />
+        <CleanTimer />
+        <LonelyTimer />
+      </View>
+      <View>
+        <View>
+          {(!HungerTimer.isHungry) ?
+            <FedDisplay /> :
+            <HungerDisplay />
+          } || {(!CleanTimer.isDirty) ?
+            <CleanDisplay /> :
+            <DirtyDisplay />
+          } || {(!LonelyTimer.isLonely) ?
+            <HappyDisplay /> :
+            <LonelyDisplay />
+          } {(!DeathTimer.isDying) ? 
+            <DefaultDisplay /> :
+            <DyingDisplay />
+          }
+        </View>
+        <View style={styles.horizontal}>
+          <Pressable
+              onPress={()=> HungerTimer.resetHungerTimer(false)}
+          >
+              <Text>Feed Me!!</Text>
+              <View style={styles.feed} />
+          </Pressable>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  clean: {
+    color: 'blue',
+    height: 30,
+    width: 30,
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  feed: {
+    color: 'green',
+    height: 30,
+    width: 30,
+    padding: 10,
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  play: {
+    color: 'red',
+    height: 30,
+    width: 30,
+    padding: 10,
+  },
+  rules: {
+    fontSize: 25,
+  },
+  timerContainer: {
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 35,
+  }
+});  
---
+++ emojiDisplay.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+
+export class CleanDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/clean.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class DirtyDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/dirty.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class FedDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/fed.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class HungryDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/hungry.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class HappyDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/playedWith.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class LonelyDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/lonely.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class DyingDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/dying.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class DeadDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/dead.jpg')} />
+            </View>
+        )
+    }
+}
+
+export class DefaultDisplay extends Component {
+    render() {
+        return (
+            <View style={styles.emojiContain}>
+                <Image  style={styles.emoji} source={require('C:/Users/packh/mobileApp/emojiPetv4/assets/default.jpg')} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    emoji: {
+        height: 150,
+        width: 150,
+    },
+    emojiContain: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 300,
+        width: 300,
+    },
+})
